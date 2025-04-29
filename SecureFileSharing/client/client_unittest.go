145b6d75026b66a4a85fcf9d0c5fbfcaf9e2f359
Diego Ortega <52830530+DiegoPlusPlus@users.noreply.github.com>
2025-04-28 18:46:58 -0700

    Update client_unittest.go

--- SecureFileSharing/client/client_unittest.go
+++ SecureFileSharing/client/client_unittest.go
@@ -1,22 +1,5 @@
 package client
 
-///////////////////////////////////////////////////
-//                                               //
-// Everything in this file will NOT be graded!!! //
-//                                               //
-///////////////////////////////////////////////////
-
-// In this unit tests file, you can write white-box unit tests on your implementation.
-// These are different from the black-box integration tests in client_test.go,
-// because in this unit tests file, you can use details specific to your implementation.
-
-// For example, in this unit tests file, you can access struct fields and helper methods
-// that you defined, but in the integration tests (client_test.go), you can only access
-// the 8 functions (StoreFile, LoadFile, etc.) that are common to all implementations.
-
-// In this unit tests file, you can write InitUser where you would write client.InitUser in the
-// integration tests (client_test.go). In other words, the "client." in front is no longer needed.
-
 import (
 	userlib "github.com/cs161-staff/project2-userlib"
 	"testing"
